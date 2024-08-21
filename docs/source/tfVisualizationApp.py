17a154db091271dff9d83af601693aa555722787
arthurmorbach <65413899+arthurmorbach@users.noreply.github.com>
2024-08-21 11:32:30 -0300

    setting page for visualization

---
+++ docs/source/tfVisualizationApp.py
@@ -0,0 +1,58 @@
+import datetime
+import time
+import numpy as np
+import math 
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+import filters
+from create_db import TF, TF_Points, Session, engine
+
+
+def main():
+    st.title("Transfer Function Analysis")
+
+    with st.sidebar:
+        st.header("Filter Selection")
+        
+        types_of_filters = ("4/4 BPF", "4/8 BPF", "4/8 BPF CC")
+        filter_type = st.selectbox(
+            label = "Select Filter", 
+            options = types_of_filters)
+        
+
+        st.header("Filter Setup")
+
+        # use the settings from the last reading, if no settings, use standard values
+        if local_session.query(TF).all():
+            st.session_state.tf_name = st.text_input(label = 'TF Name', 
+                value = local_session.query(TF).filter(TF.id == local_session.query(TF).order_by(TF.id.desc()).first().id).first().tf_name)
+
+            Cr = st.text_input(label='Rotation Capacitor (F)', value = local_session.query(TF).order_by(TF.id.desc()).first().Cr)
+            Ch = st.text_input(label='History Capacitor (F)', value = local_session.query(TF).order_by(TF.id.desc()).first().Ch)
+            beta = st.text_input(label='beta (Gain)', value = local_session.query(TF).order_by(TF.id.desc()).first().beta)
+            fs = st.text_input(label='Samplig Frequency (Hz)', value = local_session.query(TF).order_by(TF.id.desc()).first().fs)
+        else:
+            st.session_state.tf_name = st.text_input(label = 'Battery Number', value = 1)
+
+            filter_type = st.selectbox(
+                label = "Select Filter", 
+                options = types_of_filters)
+            
+            Cr = st.text_input(label='Start Frequency (Hz)', value = 75e-15)
+            Ch = st.text_input(label='End Frequency (Hz)', value = 20e-12)
+            beta = st.text_input(label='Maximum Current Amplitude (A)', value = -0.3)
+            fs = st.text_input(label='Minimum Current Amplitude (A)', value = 9.6e9)
+
+    
+if __name__ == "__main__":
+    st.set_page_config(
+		page_title = "Transfer Function Test",
+		layout = "wide"
+	)
+
+    local_session = Session(bind = engine)
+
+    main()
