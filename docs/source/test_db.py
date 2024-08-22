44d52c0c8485469b4935497f3cabc274bb7e8226
arthurmorbach <65413899+arthurmorbach@users.noreply.github.com>
2024-08-22 11:21:33 -0300

    adding to database

--- docs/source/test_db.py
+++ docs/source/test_db.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 local_session = Session(bind = engine)
 
-new = TF(tf_name = 'bpf441234', Cr = 15e-12, Ch = 10e-12, beta = 0, fs = 9.6e9, fc = 3e3, Zo = 4e3,
+new = TF(tf_name = 'bpf48CC1234', Cr = '75e-12', Ch = '20e-12', beta = '0', fs = '6e6', fc = '3e3', Zo = '4e3',
          time = datetime.utcnow())
 
 local_session.add(new)
